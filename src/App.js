7fb6213b69825be98e2670da6e31160903ccb917
dh <dohyun134@naver.com>
2025-01-08 11:40:36 +0900

    환경변수

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import NotFound from "./components/Pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="Github Finder" />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
