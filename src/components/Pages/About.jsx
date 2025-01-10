df8e7104f1c7e3b6446cde4ac1663f748d376ade
dh <dohyun134@naver.com>
2025-01-10 12:20:16 +0900

    노레퍼럴

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -6,7 +6,11 @@ function About() {
         깃허브 사용자를 찾고 프로파일을 확인할 수 있는 리엑트 앱입니다. 나의
         블로그 주소는 다음과 같습니다.
         <strong>
-          <a href="https://blog.naver.com/drv983" target="_blank">
+          <a
+            href="https://blog.naver.com/drv983"
+            target="_blank"
+            rel="noreferrer"
+          >
             {" "}
             내 블로그
           </a>
